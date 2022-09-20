e63aae14689738fc857099b05146bdacc04008d2
Astika Luintel <astild2020@gmail.com>
2022-09-20 14:59:15 -0400

    commit this

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function About() {
+function About({image = "https://images.unsplash.com/photo-1509043759401-136742328bb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"}) {
   return (
     <div id="about">
       <h2>About Me</h2>
@@ -9,3 +9,5 @@ function About() {
     </div>
   );
 }
+
+export default About;
